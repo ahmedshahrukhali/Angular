151192bea1ced799af1ff1509326763005decb62
Shahrukh <ahmedshahrukhali@gmail.com>
2021-03-28 20:38:51 -0400

    post-apocalyptic do-over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
@@ -15,6 +17,9 @@ import 'hammerjs';
   ],
   imports: [
     BrowserModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
 
     BrowserAnimationsModule,
     MatToolbarModule,

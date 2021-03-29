e1b621b923468635a88ad8571f00d5f8283cda3a
Shahrukh <ahmedshahrukhali@gmail.com>
2021-03-29 17:36:27 -0400

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatListModule } from '@angular/material/list';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatListModule
     
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

9dc02b0f628f66542e48e03c317478fd58744647
Shahrukh <ahmedshahrukhali@gmail.com>
2021-03-30 23:24:03 -0400

    assignment2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 
 import {AppRoutingModule } from './app-routing/app-routing.module';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,7 @@ import {AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule
     
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
